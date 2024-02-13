8759e22b6617febafe3211ec3e360f3146a89746
Ihor Yarovyy <Ihor.Yarovyy@ringcentral.com>
2024-02-13 21:38:13 +0200

    Use regexp to parse fields

--- pgproxy/models/proc.py
+++ pgproxy/models/proc.py
@@ -1,13 +1,14 @@
 import re
 from dataclasses import dataclass, field
+from dataclasses import replace
 from typing import List
 
 from pgproxy.console import err
 from pgproxy.models.arg import Arg
-from dataclasses import replace
 
 MARKUP_PATTERN = r'---* RESULT FIELDS -* BEGIN --([\w\W]*)---* RESULT FIELDS -* END --'
 SELECT_PATTERN = r'[Ss][Ee][Ll][Ee][Cc][Tt]([\w\W]*)[Ff][Rr][Oo][Mm]'
+FIELD_PATTERN = r'(\w*)\s*::\s*(\w*)\s*(?:[aA][sS]\s*(\w*))?'
 
 
 @dataclass(frozen=True)
@@ -27,22 +28,34 @@ class Proc:
                 if match is None:
                     match = re.search(SELECT_PATTERN, self.definition)
                 if match:
-                    for rec_field in match.group(1).split(','):
-                        name_type = rec_field.strip().split('::')
-                        name = name_type[0][name_type[0].rfind('.') + 1:]
+                    for rec_field in match.group(1).split('\n'):
+                        rec_field = rec_field.strip()
+                        if not rec_field.startswith("--") and rec_field:
+                            field_match = re.search(FIELD_PATTERN, rec_field)
+                            if field_match:
+                                query_field_name = field_match.group(1)
+                                field_type = field_match.group(2)
+                                assigned_field_name = field_match.group(3)
 
-                        if len(name_type) > 1:
-                            dtype = name_type[1]
-                            self.__record_fields.append(Arg(name, dtype.lower(), 'OUT'))
-                        else:
-                            in_arg = self.__get_arg_from_list(name)
-                            if in_arg:
-                                self.__record_fields.append(replace(in_arg, mode='OUT'))
+                                if assigned_field_name:
+                                    name = assigned_field_name
+                                else:
+                                    name = query_field_name
+
+                                if name:
+                                    if field_type:
+                                        self.__record_fields.append(Arg(name, field_type.lower(), 'OUT'))
+                                    else:
+                                        in_arg = self.__get_arg_from_list(name)
+                                        if in_arg:
+                                            self.__record_fields.append(replace(in_arg, mode='OUT'))
+                                        else:
+                                            err.print('Unknown type of field:{}, proc:{}.{}'
+                                                      .format(name, self.schema, self.name))
+                                else:
+                                    err.print('Unknown name of field record:', rec_field)
                             else:
-                                err.print('Unknown type of field:{}, proc:{}.{}'.format(name,
-                                                                                    self.schema,
-                                                                                    self.name))
-
+                                err.print('Unknown format of field record:', rec_field)
             return self.__record_fields
         else:
             return []
